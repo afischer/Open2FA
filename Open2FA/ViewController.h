463a1902bc142157ab7b747e7650096b463cad42
Andrew Fischer <afischer15@mac.com>
2018-04-03 14:41:57 -0400

    Initial Commit

---
+++ Open2FA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Open2FA
+//
+//  Created by Andrew Fischer on 4/3/18.
+//  Copyright © 2018 Andrew Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
