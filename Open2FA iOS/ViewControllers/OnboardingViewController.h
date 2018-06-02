509055a92149ff6915d94d4b8a62603cdda4bc91
Andrew Fischer <afischer15@mac.com>
2018-06-02 16:03:40 -0400

    Contextual swipe actions

---
+++ Open2FA iOS/ViewControllers/OnboardingViewController.h
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.h
+//  Open2FA
+//
+//  Created by Andrew Fischer on 6/2/18.
+//  Copyright © 2018 Andrew Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OnboardingViewController : UIViewController
+
+@end
---
+++ Open2FA iOS/ViewControllers/OnboardingViewController.m
@@ -0,0 +1,37 @@
+//
+//  OnboardingViewController.m
+//  Open2FA
+//
+//  Created by Andrew Fischer on 6/2/18.
+//  Copyright © 2018 Andrew Fischer. All rights reserved.
+//
+
+#import "OnboardingViewController.h"
+
+@interface OnboardingViewController ()
+
+@end
+
+@implementation OnboardingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
